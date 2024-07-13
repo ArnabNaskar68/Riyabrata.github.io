e7b7f4d351e7ee96c03fc1e2c337007f3acf0a1a
RiyoR10 <85172724+Riyabrata@users.noreply.github.com>
2024-07-13 21:56:14 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     console.log('DOM fully loaded and parsed');
 
-    const elements = document.querySelectorAll('.hidden');
-
-    const observer = new IntersectionObserver(entries => {
-        entries.forEach(entry => {
-            if (entry.isIntersecting) {
-                entry.target.classList.add('visible');
-                entry.target.classList.remove('hidden');
-                observer.unobserve(entry.target);
-            }
-        });
-    });
-
-    elements.forEach(element => {
-        observer.observe(element);
-    });
-
-    // Log each hidden element to confirm they are being observed
-    elements.forEach(element => {
-        console.log('Observing element:', element);
-    });
-
     // Modal logic
     const modal = document.getElementById('feedbackModal');
     const span = document.getElementsByClassName('close')[0];
